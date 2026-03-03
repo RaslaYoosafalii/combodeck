a18ac974fd0cad857715cffefe351b5cf8c9ca25
rasla yoosafali <raslayoosafali@youzefs-MacBook-Air.local>
2026-03-04 01:41:56 +0530

    google callback url change

--- config/passport.js
+++ config/passport.js
@@ -12,7 +12,10 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL
+      callbackURL:
+      process.env.NODE_ENV === 'production'
+        ? 'https://www.xn--elfloreinveil-6jb.shop/auth/google/callback'
+        : 'http://localhost:5023/auth/google/callback'
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
